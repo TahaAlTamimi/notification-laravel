a190003422ada7a7b13ce698b218a1ca6e0cf1e5
TahaAlTamimi <altamimitmo@gmail.com>
2020-01-21 10:08:11 +0200

    first commit

---
+++ resources/views/payment.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+<form method='post' action='/payment'>
+@csrf
+              <button> Make Payments </button>
+             </form>
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+//   Notification::send(request()->user(), new PaymentRecieved());
